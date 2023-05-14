8a8ce3495808f963ead9a55b3c313fed204ddd32
ditokhrsmwn <dito.kharismawan@yahoo.com>
2023-05-15 00:51:45 +0700

    update db sql table

--- build/classes/db/penggajian.sql
+++ build/classes/db/penggajian.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2017 at 03:39 PM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
+-- Generation Time: May 14, 2023 at 07:43 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,10 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cuti`
+--
+
+CREATE TABLE `cuti` (
+  `cutiID` varchar(20) NOT NULL,
+  `karyawanID` varchar(50) NOT NULL,
+  `alasan` varchar(255) NOT NULL,
+  `tgl_cuti` date NOT NULL,
+  `tgl_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `karyawan`
 --
 
-CREATE TABLE IF NOT EXISTS `karyawan` (
+CREATE TABLE `karyawan` (
   `karyawanID` int(5) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `tgl_lahir` date NOT NULL,
@@ -35,16 +50,28 @@ CREATE TABLE IF NOT EXISTS `karyawan` (
   `noHP` varchar(12) DEFAULT NULL,
   `jabatan` varchar(15) NOT NULL,
   `golongan` varchar(3) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `karyawan`
 --
 
 INSERT INTO `karyawan` (`karyawanID`, `nama`, `tgl_lahir`, `jk`, `alamat`, `noHP`, `jabatan`, `golongan`) VALUES
-(1, 'Muhammad Ali', '1997-02-03', 'L', 'Jepara', '0909098987', 'Kepala HRD', 'I'),
+(1, 'Muhammad Ali', '1997-02-03', 'L', 'Depok', '0909098987', 'Kepala HRD', 'I'),
 (2, 'Nita', '1992-06-06', 'P', 'Bandung', '9898786760', 'Sekertaris', 'I'),
 (3, 'Andi', '1992-08-21', 'L', 'Tegal', '9890807089', 'Staff IT', 'I');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kehadiran`
+--
+
+CREATE TABLE `kehadiran` (
+  `karyawanID` varchar(50) NOT NULL,
+  `jam_masuk` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `jam_pulang` timestamp(6) NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,7 +79,7 @@ INSERT INTO `karyawan` (`karyawanID`, `nama`, `tgl_lahir`, `jk`, `alamat`, `noHP
 -- Table structure for table `lembur`
 --
 
-CREATE TABLE IF NOT EXISTS `lembur` (
+CREATE TABLE `lembur` (
   `lemburID` int(5) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `jabatan` varchar(15) NOT NULL,
@@ -60,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `lembur` (
   `jml_jam` int(3) NOT NULL,
   `gaji_perjam` int(10) NOT NULL,
   `total` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lembur`
@@ -76,7 +103,7 @@ INSERT INTO `lembur` (`lemburID`, `nama`, `jabatan`, `golongan`, `jml_jam`, `gaj
 -- Table structure for table `penggajian`
 --
 
-CREATE TABLE IF NOT EXISTS `penggajian` (
+CREATE TABLE `penggajian` (
   `gajiID` int(5) NOT NULL,
   `tgl` date NOT NULL,
   `nama` varchar(30) NOT NULL,
@@ -87,14 +114,43 @@ CREATE TABLE IF NOT EXISTS `penggajian` (
   `tunjangan` int(10) NOT NULL,
   `potongan` int(10) NOT NULL,
   `gaji_bersih` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `penggajian`
 --
 
 INSERT INTO `penggajian` (`gajiID`, `tgl`, `nama`, `jabatan`, `golongan`, `gapok`, `gaji_lembur`, `tunjangan`, `potongan`, `gaji_bersih`) VALUES
-(2, '2017-06-10', 'Muhammad Ali', 'Kepala HRD', 'I', 4000000, 120000, 200000, 50000, 4270000);
+(2, '2017-06-10', 'Muhammad Ali', 'Kepala HRD', 'I', 4000000, 120000, 200000, 50000, 4270000),
+(3, '2023-05-05', 'Nita', 'Sekertaris', 'I', 10000000, 45000, 500000, 2500000, 8045000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `potongan`
+--
+
+CREATE TABLE `potongan` (
+  `potonganID` varchar(50) NOT NULL,
+  `karyawanID` varchar(50) NOT NULL,
+  `jenis_potongan` varchar(50) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tunjangan`
+--
+
+CREATE TABLE `tunjangan` (
+  `tunjanganID` varchar(50) NOT NULL,
+  `karyawanID` varchar(50) NOT NULL,
+  `jenis_tunjangan` varchar(50) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -102,11 +158,11 @@ INSERT INTO `penggajian` (`gajiID`, `tgl`, `nama`, `jabatan`, `golongan`, `gapok
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `noID` int(3) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -151,22 +207,27 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `karyawanID` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `karyawanID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `lembur`
 --
 ALTER TABLE `lembur`
-  MODIFY `lemburID` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `lemburID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `penggajian`
 --
 ALTER TABLE `penggajian`
-  MODIFY `gajiID` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `gajiID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `noID` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `noID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
