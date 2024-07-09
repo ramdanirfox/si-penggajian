242c76e194122977ef5713fb2ef3068acb935c66
iramdan461 <iramdan461@gmail.com>
2024-07-10 06:44:39 +0700

    Perbaikan Tampilan Karyawan

--- src/Form/Form_Karyawan.java
+++ src/Form/Form_Karyawan.java
@@ -675,7 +675,7 @@ public class Form_Karyawan extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jButton4))
                             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap(12, Short.MAX_VALUE))
+                .addContainerGap(55, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
